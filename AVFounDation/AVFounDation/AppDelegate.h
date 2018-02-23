12b80a5ff8d242f85e351ffd25a3c60ba7f210bf
xiaoaoyintian <zhangjump@yeah.net>
2018-02-23 10:52:49 +0800

    Add files via upload

---
+++ AVFounDation/AVFounDation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVFounDation
+//
+//  Created by home on 2018/2/23.
+//  Copyright © 2018年 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVFounDation/AVFounDation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVFounDation
+//
+//  Created by home on 2018/2/23.
+//  Copyright © 2018年 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
