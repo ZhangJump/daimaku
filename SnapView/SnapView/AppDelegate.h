d4201bf962d591ae72d7ca4b9434e5a36e671e75
xiaoaoyintian <zhangjump@yeah.net>
2018-03-06 15:14:49 +0800

    Add files via upload

---
+++ SnapView/SnapView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnapView
+//
+//  Created by home on 2018/3/6.
+//  Copyright © 2018年 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnapView/SnapView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnapView
+//
+//  Created by home on 2018/3/6.
+//  Copyright © 2018年 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
